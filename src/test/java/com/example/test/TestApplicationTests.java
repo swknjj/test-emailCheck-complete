246f7597625ced2f39d0e31178d4a51e88808a5d
swknjj <sinwook9255@gmail.com>
2023-06-25 04:46:59 +0900

    테스트 코드 작성

--- src/test/java/com/example/test/TestApplicationTests.java
+++ src/test/java/com/example/test/TestApplicationTests.java
@@ -1,13 +1,28 @@
 package com.example.test;
 
+import com.example.test.Service.MailService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.mail.MessagingException;
+import java.io.UnsupportedEncodingException;
 
 @SpringBootTest
 class TestApplicationTests {
+    @Autowired
+    private MailService mailService;
 
     @Test
     void contextLoads() {
+    }
+    
+    @Test
+    @DisplayName("이메일 인증 테스트")
+    public void emailCheckTest() throws MessagingException, UnsupportedEncodingException {
+        String email_check_code = mailService.sendEmail("lsw3674@gmail.com");
+        System.out.println("email_check_code = " + email_check_code);
     }
 
 }
